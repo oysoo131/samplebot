ba7b3b1bd0c23a3aa49eb8c45604bfbabe0d493f
oysoo131 <104242001+oysoo131@users.noreply.github.com>
2024-03-22 22:10:25 +0900

    🎫0.01 업데이트!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import discord
+client = discord.Client(intents=discord.Intents.default())
+tree = discord.app_commands.CommandTree(client)
+TOKEN = "토큰 넣기"
+@client.event
+async def on_ready():
+    print(f"봇 {client.user} 이 켜졌습니다 현제 셈플 봇 버전 0.01 all copyright to bob_love 저희 셈플을 사용해 주셔서 감사합니다")
+    await tree.sync()
+    ac=discord.activity.Activity(name="셈플봇!",type=discord.ActivityType.listening)
+    await client.change_presence(activity=ac)
+client.run(TOKEN)
